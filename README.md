6afb8819d29040fd4a244bb2e8088eebf106e3c5
Guillaume Havard <guillaumeh@anotherbrain.ai>
2024-11-28 13:53:30 +0100

    feat: Add csv checker for some IPTC fields

--- README.md
+++ README.md
@@ -6,13 +6,65 @@ Tool to concatenate several csv files and check their content.
 
 ### If you are on linux(debian)
 
+```bash
+# To cross-compile to Windows
 sudo apt install mingw-w64
 rustup target add x86_64-pc-windows-gnu
+```
 
 build:
+```bash
 cargo build --release
+```
 
 build for windows:
+```bash
 cargo build --release --target x86_64-pc-windows-gnu
+```
+
+## Usage
+
+```bash
+cargo run <Directory of csv file>
+```
+
+the output file will be called ``./output.csv``.
+
+### Exemple of result for check
+
+```bash
+CSV files concatenated successfully!
+Line 3
+        * Sup category is 'FNAC', it should be 'MQB - Iconotheque'
+StringRecord(["2", "2.545 Mio", "3200 DPI", " 2_01", "FNAC", "voiture", "evian"])
+
+Line 4
+        * Field 'toto' is empty
+StringRecord(["3", "3.5 Mio", "3200 DPI", " 3_01", "MQB - Iconotheque", "", "evian"])
+
+Line 5
+        * Size '1.9 Mio' is below the minimum '2 Mio'
+StringRecord(["4", "1.9 Mio", "3200 DPI", " 4_01", "MQB - Iconotheque", "ecrevisse", "evian"])
+
+Line 6
+        * Object name '99_01' does not match filename '5'
+StringRecord(["5", "2.9 Mio", "3200 DPI", " 99_01", "MQB - Iconotheque", "pantoufle", "evian"])
+
+Line 7
+        * Source is 'vittel', it should be 'evian'
+StringRecord(["6", "2.5 Mio", "3200 DPI", " 6_01", "MQB - Iconotheque", "licorne", "vittel"])
+
+Line 8
+        * Width is '1600 DPI', it should be '3200 DPI'
+StringRecord(["7", "2.5 Mio", "1600 DPI", " 7_01", "MQB - Iconotheque", "Ane", "evian"])
+
+Line 9
+        * Size '5.5 Mio' is above the maximum '4 Mio'
+        * Object name '88_01' does not match filename '8'
+StringRecord(["8", "5.5 Mio", "3200 DPI", " 88_01", "MQB - Iconotheque", "poufsoufle", "evian"])
+
+CSV file validated successfully!
+```
+
 
 
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,19 @@ use std::path::Path;
 const OUTPUT_DELIMITER: u8 = b',';
 const RAW_DELIMITER: u8 = b':';
 
+const CATEGORY: &str = "MQB - Iconotheque";
+const SOURCE: &str = "evian";
+const MIN_SIZE: f64 = 2.0; // in Mio
+const MAX_SIZE: f64 = 4.0; // in Mio
+const WIDTH_DPI: &str = "3200 DPI";
+
+const COL_FILENAME: &str = "Filename";
+const COL_SIZE: &str = "Size";
+const COL_WIDTH: &str = "Width";
+const COL_OBJECT_NAME: &str = "IPTC:ObjectName";
+const COL_SUP_CATEGORY: &str = "IPTC:Sup.";
+const COL_SOURCE: &str = "IPTC:Source";
+
 fn concatenate_csv_files(
     input_folder: &str,
     output_file: &str,
@@ -46,6 +59,101 @@ fn concatenate_csv_files(
     Ok(())
 }
 
+fn validate_csv(file_path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    // Use ReaderBuilder to configure the reader with a custom delimiter
+    let mut reader = ReaderBuilder::new()
+        .delimiter(OUTPUT_DELIMITER)
+        .has_headers(true) // Indicate that the CSV file has headers
+        .from_path(file_path)?;
+
+    let headers = reader.headers()?.clone();
+
+    // Get column indices based on header names
+    let col_filename = headers.iter().position(|h| h == COL_FILENAME).unwrap();
+    let col_size = headers.iter().position(|h| h == COL_SIZE).unwrap();
+    let col_width = headers.iter().position(|h| h == COL_WIDTH).unwrap();
+    let col_object_name = headers.iter().position(|h| h == COL_OBJECT_NAME).unwrap();
+    let col_sup_category = headers.iter().position(|h| h == COL_SUP_CATEGORY).unwrap();
+    let col_source = headers.iter().position(|h| h == COL_SOURCE).unwrap();
+
+    // Iterate through rows
+    for (line_number, result) in reader.records().enumerate() {
+        let record = result?;
+        let mut errors = Vec::new();
+
+        for (i, value) in record.iter().enumerate() {
+            if value.trim().is_empty() {
+                errors.push(format!("Field '{}' is empty", headers.get(i).unwrap()));
+            }
+        }
+
+        let size = record[col_size].trim();
+        if let Some(size_value) = size.strip_suffix(" Mio") {
+            if let Ok(size_parsed) = size_value.parse::<f64>() {
+                if size_parsed < MIN_SIZE {
+                    errors.push(format!(
+                        "Size '{} Mio' is below the minimum '{} Mio'",
+                        size_parsed, MIN_SIZE
+                    ));
+                } else if size_parsed > MAX_SIZE {
+                    errors.push(format!(
+                        "Size '{} Mio' is above the maximum '{} Mio'",
+                        size_parsed, MAX_SIZE
+                    ));
+                }
+            } else {
+                errors.push(format!("Invalid size format: {}", size_value));
+            }
+        }
+
+        let width = record[col_width].trim();
+        if width != WIDTH_DPI {
+            errors.push(format!(
+                "Width is '{}', it should be '{}'",
+                width, WIDTH_DPI
+            ));
+        }
+
+        let object_name = record[col_object_name].trim();
+        let object_name_prefix = object_name.split('_').next().unwrap_or("");
+        let filename = record[col_filename].trim();
+        if object_name_prefix != filename {
+            errors.push(format!(
+                "Object name '{}' does not match filename '{}'",
+                object_name.trim(),
+                filename
+            ));
+        }
+
+        let sup_category = record[col_sup_category].trim();
+        if sup_category != CATEGORY {
+            errors.push(format!(
+                "Sup category is '{}', it should be '{}'",
+                sup_category, CATEGORY
+            ));
+        }
+
+        let source = record[col_source].trim();
+        if source != SOURCE {
+            errors.push(format!("Source is '{}', it should be '{}'", source, SOURCE));
+        }
+
+        if !errors.is_empty() {
+            println!(
+                "Line {}",
+                line_number + 2, // Add 2 to account for header and 0-based index
+            );
+            for error in errors {
+                println!("\t* {}", error);
+            }
+            println!("{:?}", record);
+            println!();
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
     let input_folder = env::args().nth(1);
     let input_folder = input_folder.as_deref().unwrap_or("./tests/data/raw");
@@ -59,6 +167,11 @@ fn main() {
 
     match concatenate_csv_files(input_folder, output_file) {
         Ok(_) => println!("CSV files concatenated successfully!"),
+        Err(e) => eprintln!("Error: {}", e),
+    }
+
+    match validate_csv(Path::new(output_file)) {
+        Ok(_) => println!("CSV file validated successfully!"),
         Err(e) => eprintln!("Error: {}", e),
     }
 }
